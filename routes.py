e61198e889e0554dd4600cd7c43c97f4c2400d8d
Tyler <tylerkershner@gmail.com>
2017-06-23 13:01:54 -0400

    Added first page GIF filter for faster page load

--- routes.py
+++ routes.py
@@ -26,13 +26,51 @@ def index():
         if 'hero-vid' not in item.key:
             s3_url = '%s/%s/%s' % (S3_BASE_URL, bucket_name, item.key)
             image_urls.append(s3_url)
-
     shuffle(image_urls)
+
+    found_gifs = False
+    first_page = image_urls[:15]
+    first_page_gifs = []
+    for i, url in enumerate(first_page):
+        if url.endswith('.gif'):
+            found_gifs = True
+            first_page_gif = {
+                'index': i,
+                'url': url
+            }
+            first_page_gifs.append(first_page_gif)
+
+    if found_gifs:
+        image_urls = filter_first_page_gifs(image_urls, first_page_gifs)
+
     return render_template('home.html',
                            hero_key=hero_key,
                            image_urls=json.dumps(image_urls),
                            bucket_name=bucket_name,
                            s3_base_url=S3_BASE_URL)
+
+
+# Replace first page GIFs with next available non-GIFs for faster initial page load
+def filter_first_page_gifs(image_urls, first_page_gifs):
+    non_gifs = []
+    after_first_page = image_urls[15:]
+    for url in after_first_page:
+        if len(non_gifs) < len(first_page_gifs):
+            if not url.endswith('.gif'):
+                non_gif = {
+                    'index': image_urls.index(url),
+                    'url': url
+                }
+                non_gifs.append(non_gif)
+        else:
+            break
+
+    for i, non_gif in enumerate(non_gifs):
+        first_page_gif = first_page_gifs[i]
+        # Swap first page GIF with next non-GIF from image_urls
+        image_urls[first_page_gif['index']], image_urls[non_gif['index']] = non_gif['url'], first_page_gif['url']
+
+    return image_urls
 
 
 # Development server
